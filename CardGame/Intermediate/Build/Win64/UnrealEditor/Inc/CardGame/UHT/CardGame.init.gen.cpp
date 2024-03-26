674183f73789e73ab4f1bc968d53e6878511b613
Wiktor Janik <92025939+WedgeManWik@users.noreply.github.com>
2024-03-26 03:30:08 +0000

    setup

--- CardGame/Intermediate/Build/Win64/UnrealEditor/Inc/CardGame/UHT/CardGame.init.gen.cpp
+++ CardGame/Intermediate/Build/Win64/UnrealEditor/Inc/CardGame/UHT/CardGame.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeCardGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB56015EF,
+				0xFE6BF48D,
 				0xEC37F3BD,
 				METADATA_PARAMS(nullptr, 0)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeCardGame_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_CardGame.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CardGame(Z_Construct_UPackage__Script_CardGame, TEXT("/Script/CardGame"), Z_Registration_Info_UPackage__Script_CardGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB56015EF, 0xEC37F3BD));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CardGame(Z_Construct_UPackage__Script_CardGame, TEXT("/Script/CardGame"), Z_Registration_Info_UPackage__Script_CardGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xFE6BF48D, 0xEC37F3BD));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CardGame/Intermediate/Build/Win64/x64/CardGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.34.31943
+#include "D:/GitHub/MobileCardGame/CardGame/Intermediate/Build/Win64/x64/CardGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
