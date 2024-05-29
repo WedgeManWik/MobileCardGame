5e42bb7fe8c3dfcbcf0e2ff4aa4c35877044cfe5
WedgeManWik <j012100l@student.staffs.ac.uk>
2024-05-29 14:58:02 +0100

    Fixed Spawn Points

---
+++ CardGame/Intermediate/Build/Win64/UnrealEditor/Inc/CardGame/UHT/CardGame.gen.cpp
@@ -0,0 +1,83 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "CardGame/CardGame.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeCardGame() {}
+// Cross Module References
+	CARDGAME_API UEnum* Z_Construct_UEnum_CardGame_EAbilityInputID();
+	UPackage* Z_Construct_UPackage__Script_CardGame();
+// End Cross Module References
+	static FEnumRegistrationInfo Z_Registration_Info_UEnum_EAbilityInputID;
+	static UEnum* EAbilityInputID_StaticEnum()
+	{
+		if (!Z_Registration_Info_UEnum_EAbilityInputID.OuterSingleton)
+		{
+			Z_Registration_Info_UEnum_EAbilityInputID.OuterSingleton = GetStaticEnum(Z_Construct_UEnum_CardGame_EAbilityInputID, (UObject*)Z_Construct_UPackage__Script_CardGame(), TEXT("EAbilityInputID"));
+		}
+		return Z_Registration_Info_UEnum_EAbilityInputID.OuterSingleton;
+	}
+	template<> CARDGAME_API UEnum* StaticEnum<EAbilityInputID>()
+	{
+		return EAbilityInputID_StaticEnum();
+	}
+	struct Z_Construct_UEnum_CardGame_EAbilityInputID_Statics
+	{
+		static const UECodeGen_Private::FEnumeratorParam Enumerators[];
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Enum_MetaDataParams[];
+#endif
+		static const UECodeGen_Private::FEnumParams EnumParams;
+	};
+	const UECodeGen_Private::FEnumeratorParam Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::Enumerators[] = {
+		{ "EAbilityInputID::None", (int64)EAbilityInputID::None },
+		{ "EAbilityInputID::Confirm", (int64)EAbilityInputID::Confirm },
+		{ "EAbilityInputID::Cancel", (int64)EAbilityInputID::Cancel },
+		{ "EAbilityInputID::FireAbility", (int64)EAbilityInputID::FireAbility },
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::Enum_MetaDataParams[] = {
+		{ "BlueprintType", "true" },
+		{ "Cancel.Name", "EAbilityInputID::Cancel" },
+		{ "Confirm.Name", "EAbilityInputID::Confirm" },
+		{ "FireAbility.Name", "EAbilityInputID::FireAbility" },
+		{ "ModuleRelativePath", "CardGame.h" },
+		{ "None.Name", "EAbilityInputID::None" },
+	};
+#endif
+	const UECodeGen_Private::FEnumParams Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::EnumParams = {
+		(UObject*(*)())Z_Construct_UPackage__Script_CardGame,
+		nullptr,
+		"EAbilityInputID",
+		"EAbilityInputID",
+		Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::Enumerators,
+		UE_ARRAY_COUNT(Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::Enumerators),
+		RF_Public|RF_Transient|RF_MarkAsNative,
+		EEnumFlags::None,
+		(uint8)UEnum::ECppForm::EnumClass,
+		METADATA_PARAMS(Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::Enum_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::Enum_MetaDataParams))
+	};
+	UEnum* Z_Construct_UEnum_CardGame_EAbilityInputID()
+	{
+		if (!Z_Registration_Info_UEnum_EAbilityInputID.InnerSingleton)
+		{
+			UECodeGen_Private::ConstructUEnum(Z_Registration_Info_UEnum_EAbilityInputID.InnerSingleton, Z_Construct_UEnum_CardGame_EAbilityInputID_Statics::EnumParams);
+		}
+		return Z_Registration_Info_UEnum_EAbilityInputID.InnerSingleton;
+	}
+	struct Z_CompiledInDeferFile_FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGame_h_Statics
+	{
+		static const FEnumRegisterCompiledInInfo EnumInfo[];
+	};
+	const FEnumRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGame_h_Statics::EnumInfo[] = {
+		{ EAbilityInputID_StaticEnum, TEXT("EAbilityInputID"), &Z_Registration_Info_UEnum_EAbilityInputID, CONSTRUCT_RELOAD_VERSION_INFO(FEnumReloadVersionInfo, 2039570255U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGame_h_721589254(TEXT("/Script/CardGame"),
+		nullptr, 0,
+		nullptr, 0,
+		Z_CompiledInDeferFile_FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGame_h_Statics::EnumInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGame_h_Statics::EnumInfo));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CardGame/Intermediate/Build/Win64/UnrealEditor/Inc/CardGame/UHT/CardGamePlayerController.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "CardGamePlayerController.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CARDGAME_CardGamePlayerController_generated_h
+#error "CardGamePlayerController.generated.h already included, missing '#pragma once' in CardGamePlayerController.h"
+#endif
+#define CARDGAME_CardGamePlayerController_generated_h
+
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_SPARSE_DATA
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_RPC_WRAPPERS
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_ACCESSORS
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACardGamePlayerController(); \
+	friend struct Z_Construct_UClass_ACardGamePlayerController_Statics; \
+public: \
+	DECLARE_CLASS(ACardGamePlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/CardGame"), NO_API) \
+	DECLARE_SERIALIZER(ACardGamePlayerController)
+
+
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_INCLASS \
+private: \
+	static void StaticRegisterNativesACardGamePlayerController(); \
+	friend struct Z_Construct_UClass_ACardGamePlayerController_Statics; \
+public: \
+	DECLARE_CLASS(ACardGamePlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/CardGame"), NO_API) \
+	DECLARE_SERIALIZER(ACardGamePlayerController)
+
+
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACardGamePlayerController(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACardGamePlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACardGamePlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACardGamePlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACardGamePlayerController(ACardGamePlayerController&&); \
+	NO_API ACardGamePlayerController(const ACardGamePlayerController&); \
+public: \
+	NO_API virtual ~ACardGamePlayerController();
+
+
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACardGamePlayerController(ACardGamePlayerController&&); \
+	NO_API ACardGamePlayerController(const ACardGamePlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACardGamePlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACardGamePlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACardGamePlayerController) \
+	NO_API virtual ~ACardGamePlayerController();
+
+
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_14_PROLOG
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_SPARSE_DATA \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_RPC_WRAPPERS \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_ACCESSORS \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_INCLASS \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_SPARSE_DATA \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_ACCESSORS \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_INCLASS_NO_PURE_DECLS \
+	FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CARDGAME_API UClass* StaticClass<class ACardGamePlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_GitHub_MobileCardGame_CardGame_Source_CardGame_CardGamePlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
